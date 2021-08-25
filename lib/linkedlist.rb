d48c1dd6d8ff08cb9c3fd1d8f35399a3d58d5735
Paul-Henri BONNEMENT <paulhenri.bonnement@gmail.com>
2021-08-25 21:55:20 +0000

    Added basic documentation generated by Yard Revamped size function to eliminate possible errors

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -1,164 +1,176 @@
-class LinkedList
+# frozen_string_literal: true
 
+# @author Paul-Henri BONNEMENT
+class LinkedList
   attr_reader :list
 
-  public
+  # Constructor of the class
+  # @param init_value [Integer] Value of the first node
   def initialize(init_value)
     @nb_nodes = 1
     @list = Node.new
     @list.value = init_value
     @current_node = @list
-  end 
+  end
 
-  public 
+  # Push a value at the end of the list
+  # @param value [Integer] Value of the new node
   def append(value)
-    @nb_nodes += 1
     @current_node = @list
-    while @current_node.next != nil 
-      @current_node = @current_node.next
-    end 
+    @current_node = @current_node.next until @current_node.next.nil?
     @current_node.next = Node.new
     @current_node = @current_node.next
     @current_node.value = value
-  end 
+  end
 
-  public
+  # Place a value at the beginning of the list
+  # @param value [Integer] value of the new node
   def prepend(value)
-    @nb_nodes += 1
-    new_node = Node.new 
-    new_node.value = value 
-    new_node.next = @list 
+    new_node = Node.new
+    new_node.value = value
+    new_node.next = @list
     @list = new_node
-  end 
+  end
 
-  public
+  # Return the size of the list
+  # @return [Integer] Number of nodes
   def size
-    return @nb_nodes
-  end 
+    calc_size
+  end
 
-  public
+  private
+
+  #  Calculate the number of node by travesing the list
+  #  @return [Integer] Number of nodes
   def calc_size
-    nb = 0
-    @current_node = @list
-    while @current_node != nil 
+    nb = 1
+    current_node = @list
+    until current_node.nil?
       nb += 1
-      @current_node = @current_node.next
-    end
-    return nb
-  end 
-
-  public
-  def head 
-    return @list
-  end 
-
-  public
-  def tail 
-   current_node = @list
-    while current_node.next != nil 
       current_node = current_node.next
-    end 
-   return current_node
-  end 
-  
+    end
+    nb
+  end
+
   public
+
+  # Return first node
+  # @return [Node] First node
+  def head
+    @list
+  end
+
+  # Return last node
+  # @return [Node] Last node
+  def tail
+    current_node = @list
+    current_node = current_node.next until current_node.next.nil?
+    current_node
+  end
+
+  # Return node located at given index
+  # @param index [Integer] location
+  # @return [Node, nil] Node if found, nil if not located (Index out of bound)
   def at(index)
-    if index <= 0 || index > self.size 
-      return nil
-    else 
+    if index <= 0 || index > size
+      nil
+    else
       current_node = @list
-      nb=1
-      while nb != index 
+      nb = 1
+      while nb != index
         current_node = current_node.next
-        nb+=1
-      end 
-      return current_node
-    end 
-  end 
+        nb += 1
+      end
+      current_node
+    end
+  end
 
-  public
-  def pop 
-    size = self.size 
-    if size > 1 
-      self.at(self.size - 1).next = nil
-    end 
-  end 
+  # Pop the last node (removes it)
+  def pop
+    size = self.size
+    at(self.size - 1).next = nil if size > 1
+  end
 
+  # Check if given element is contained in the list
+  # @return [true, false]
   def contains?(value)
-    current = @list 
-    while current != nil 
-      return true if current.value == value
-      current = current.next
-    end 
-    return false
-  end 
-
-  def find(value) 
-    current = @list 
-    while current != nil 
-      return true if current.value == value
-      current = current.next
-    end 
-    return false
-  end 
-
-  def to_s
-    to_s_string = ""
     current = @list
-    while current != nil
+    until current.nil?
+      return true if current.value == value
+
+      current = current.next
+    end
+    false
+  end
+
+  # Return first node with corresponding value
+  # @param value [Integer] value to be searched for
+  # @return [Node, nil]
+  def find(value)
+    current = @list
+    until current.nil?
+      return true if current.value == value
+
+      current = current.next
+    end
+    false
+  end
+
+  # To String modification
+  # @return [String] String representation of the list (x) -> (y) -> [..] -> nil
+  def to_s
+    to_s_string = ''
+    current = @list
+    until current.nil?
       to_s_string << "(#{current.value}) -> "
       current = current.next
-    end 
-    to_s_string << "nil"
-    return to_s_string
-  end 
+    end
+    to_s_string << 'nil'
+    to_s_string
+  end
 
-  public
-  def print_list 
-    @current_node = @list
-    while @current_node != nil 
-      puts @current_node.value.to_s << " -> "
-      @current_node = @current_node.next
-    end 
-  end 
-
-  public 
+  # Insert a new node at given location
+  # @param index [Integer] location of new node
+  # @param value [Integer] Value for the new node
+  # @return [true, false] Result of the insertion
   def insert_at(index, value)
-    return false if index < 0 || index > self.size
-    if index > 1 && index <= self.size
-      replaced_node = self.at(index)
-      new_node = Node.new 
+    return false if index.negative? || index > size
+
+    if index > 1 && index <= size
+      replaced_node = at(index)
+      new_node = Node.new
       new_node.value = value
       new_node.next = replaced_node
-      self.at(index-1).next = new_node
+      at(index - 1).next = new_node
     else
-      self.prepend(value)
-    end 
-    return true
-  end 
+      prepend(value)
+    end
+    true
+  end
 
-  public 
+  # Removes node at given location
+  # @param index [Integer] Index of the node to be removed
+  # @return [true, false] Issue of the command
   def remove_at(index)
-    return false if index < 0 || index > self.size
-    if index > 1 
-      new_next_node = self.at(index+1)
-      self.at(index-1).next = new_next_node
+    return false if index.negative? || index > size
+
+    if index > 1
+      new_next_node = at(index + 1)
+      at(index - 1).next = new_next_node
     else
       @list = @list.next
-    end 
-    return true
-  end 
-end 
+    end
+    true
+  end
+end
 
-class Node 
-  
-  attr_accessor :value 
-  attr_accessor :next
+# Represent a Node, used in LinkedList class
+class Node
+  attr_accessor :value, :next
 
+  # Constructor
   def initialize
     @value = nil
     @next = @nil
-  end 
-
-end 
-
+  end
+end
--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,164 +1,165 @@
-require "linkedlist"
+# frozen_string_literal: true
+
+require 'linkedlist'
 describe LinkedList do
-  describe ".initialize" do 
-    context "Initialize a list with a value" do
-      it "should be readable on the @list.value reader" do
+  describe '.initialize' do
+    context 'Initialize a list with a value' do
+      it 'should be readable on the @list.value reader' do
         tlist = LinkedList.new(3)
         expect(tlist.list.value).to eq(3)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".tail" do 
-    context "Given a list with 1 node" do 
-      it "should output the value of the first node" do 
+  describe '.tail' do
+    context 'Given a list with 1 node' do
+      it 'should output the value of the first node' do
         tlist = LinkedList.new(3)
         expect(tlist.tail.value).to eq(3)
-      end 
-    end 
+      end
+    end
 
-    context "Given a list with more than 1 node" do 
-      it "should output the value of the last inserted node" do 
+    context 'Given a list with more than 1 node' do
+      it 'should output the value of the last inserted node' do
         tlist = LinkedList.new(5)
         last_inserted = 0
-        for i in 1..rand(1..50) 
+        (1..rand(1..50)).each do |_i|
           last_inserted = rand(1..100)
           tlist.append(last_inserted)
-        end 
+        end
         expect(tlist.tail.value).to eq(last_inserted)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".head" do 
-    context "Given a list with one value" do 
-      it "should return the value of the first node " do
+  describe '.head' do
+    context 'Given a list with one value' do
+      it 'should return the value of the first node ' do
         tlist = LinkedList.new(5)
         expect(tlist.head.value).to eq(5)
-      end 
-    end 
-    context "Given a list with multiple value" do 
-      it "should return the value of the first node" do
+      end
+    end
+    context 'Given a list with multiple value' do
+      it 'should return the value of the first node' do
         tlist = LinkedList.new(5)
         last_inserted = 0
-        for i in 1..rand(1..50) 
+        (1..rand(1..50)).each do |_i|
           last_inserted = rand(1..100)
           tlist.append(last_inserted)
-        end 
+        end
         expect(tlist.head.value).to eq(5)
-      end 
-    end 
+      end
+    end
 
-    context "Given a list, after a random number of call to .prepend" do
-      it "should return the last value prepended" do
+    context 'Given a list, after a random number of call to .prepend' do
+      it 'should return the last value prepended' do
         tlist = LinkedList.new(5)
         last_inserted = 0
-        for i in 1..rand(1..50) 
+        (1..rand(1..50)).each do |_i|
           last_inserted = rand(1..100)
           tlist.prepend(last_inserted)
-        end 
+        end
         expect(tlist.head.value).to eq(last_inserted)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".prepend" do 
-    context "Given a list" do 
-      it "should place the value in front of the list" do
+  describe '.prepend' do
+    context 'Given a list' do
+      it 'should place the value in front of the list' do
         tlist = LinkedList.new(3)
         tlist.prepend(5)
         expect(tlist.head.value).to eq(5)
         expect(tlist.tail.value).to eq(3)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".append" do
-    context "Append a value to a list" do 
-      it "tail should be eq to the value" do 
+  describe '.append' do
+    context 'Append a value to a list' do
+      it 'tail should be eq to the value' do
         tlist = LinkedList.new(3)
         tlist.append(8)
         expect(tlist.tail.value).to eq(8)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".at" do 
-    context "Given a list initialized with a random number of nodes" do 
-      it "should return a valid node if index is between boundaries" do
+  describe '.at' do
+    context 'Given a list initialized with a random number of nodes' do
+      it 'should return a valid node if index is between boundaries' do
         # We use a hash to store values and position of nodes pos => value
         tlist = LinkedList.new(1)
         nb_values = rand(10..100)
-        for i in 2..nb_values
+        (2..nb_values).each do |i|
           tlist.append(i)
-        end 
+        end
         hazard_test = rand(nb_values)
         expect(tlist.at(hazard_test).class).to eq(Node)
-      end 
+      end
 
-      it "should return a nil value if index is 0 or out of bound" do 
+      it 'should return a nil value if index is 0 or out of bound' do
         tlist = LinkedList.new(1)
         nb_values = rand(10..100)
-        for i in 2..nb_values
+        (2..nb_values).each do |i|
           tlist.append(i)
-        end 
-        hazard_test = nb_values+10
-        expect(tlist.at(hazard_test)).to eq(nil) 
-      end 
-    end     
-  end 
+        end
+        hazard_test = nb_values + 10
+        expect(tlist.at(hazard_test)).to eq(nil)
+      end
+    end
+  end
 
-  describe ".pop" do 
-    context "Given a list initialized with 2+ values" do 
-      it "should delete the last value" do 
+  describe '.pop' do
+    context 'Given a list initialized with 2+ values' do
+      it 'should delete the last value' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
-        tlist.pop 
+        tlist.pop
         expect(tlist.tail.value).to eq(2)
-      end 
-    end 
-    context "Given a list with 1 value" do 
-      it "should do nothing" do
+      end
+    end
+    context 'Given a list with 1 value' do
+      it 'should do nothing' do
         tlist = LinkedList.new(1)
-        tlist.pop 
+        tlist.pop
         expect(tlist.tail.value).to eq(1)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  describe ".contains?" do 
-    context "Given a list with a node containing 1, 2, 3" do 
-      it "should return true when called with argument 3" do
+  describe '.contains?' do
+    context 'Given a list with a node containing 1, 2, 3' do
+      it 'should return true when called with argument 3' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
         expect(tlist.contains?(3)).to eq(true)
-      end 
-      it "should return false when called with argument 6" do 
+      end
+      it 'should return false when called with argument 6' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
         expect(tlist.contains?(6)).to eq(false)
-      end 
+      end
+    end
+  end
 
-    end 
-  end 
-
-  describe ".to_s" do 
-    context "Given a list with nodes containing 1, 2, 3" do 
-      it "should return (1) -> (2) -> (3) -> nil " do
+  describe '.to_s' do
+    context 'Given a list with nodes containing 1, 2, 3' do
+      it 'should return (1) -> (2) -> (3) -> nil ' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
-        expect(tlist.to_s).to eq("(1) -> (2) -> (3) -> nil")
-      end 
-    end 
+        expect(tlist.to_s).to eq('(1) -> (2) -> (3) -> nil')
+      end
+    end
   end
 
-  describe ".insert_at" do
-    context "Given a list with nodes containing 1, 2, 3, 4, 5" do 
-      it "should insert between 2 et 3 when asked to insert at 3 " do
+  describe '.insert_at' do
+    context 'Given a list with nodes containing 1, 2, 3, 4, 5' do
+      it 'should insert between 2 et 3 when asked to insert at 3 ' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
@@ -168,13 +169,13 @@ describe LinkedList do
         expect(tlist.at(3).value).to eq(9)
         expect(tlist.at(2).next).to be(tlist.at(3))
         expect(tlist.at(3).next).to be(tlist.at(4))
-      end 
-    end 
+      end
+    end
   end
 
-  describe ".remove_at" do
-    context "Given a list with nodes containing 1, 2, 3, 4, 5" do 
-      it "should remove the third value when asked to do " do
+  describe '.remove_at' do
+    context 'Given a list with nodes containing 1, 2, 3, 4, 5' do
+      it 'should remove the third value when asked to do ' do
         tlist = LinkedList.new(1)
         tlist.append(2)
         tlist.append(3)
@@ -184,6 +185,6 @@ describe LinkedList do
         expect(tlist.at(3).value).to eq(4)
         expect(tlist.at(2).next).to be(tlist.at(3))
       end
-    end 
-  end 
-end 
+    end
+  end
+end
